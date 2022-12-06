57d3f471bacedc024542fa3fb7e8a826ba96ff7e
PC-202210311905\Administrator <fangxiaoyun@yuctime.com>
2022-12-06 14:37:43 +0800

    进行混淆

--- app/src/main/java/com/yucfangxiaoyun/gitdemo/HunXiaoActivity.kt
+++ app/src/main/java/com/yucfangxiaoyun/gitdemo/HunXiaoActivity.kt
@@ -3,10 +3,10 @@ package com.yucfangxiaoyun.gitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HunXiaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hun_xiao)
 
         val a = 0
         val b = 0
